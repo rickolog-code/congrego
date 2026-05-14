749e093e3d4e7033bce47b2a0bc5e75a390f7e20
Dominic Billiau <dominicbilliau@gmail.com>
2026-05-14 14:00:06 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,14 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import BottomNav from './BottomNav';
-import { JungleBottom, MonkeyVine } from './JungleDecoration';
+import LeafDecoration from './LeafDecoration';
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-background relative overflow-hidden">
-      {/* Monkey on vine - top right, shifted slightly right */}
-      <MonkeyVine className="top-0 right-[-8px] z-0" />
-      {/* Bottom jungle scene */}
-      <JungleBottom />
+    <div className="min-h-screen bg-background jungle-bg relative overflow-hidden">
+      <LeafDecoration className="text-primary -top-10 -left-10 w-40 h-40 rotate-[-30deg]" />
+      <LeafDecoration className="text-primary -bottom-16 -right-8 w-48 h-48 rotate-[150deg]" />
       <div className="relative z-10 pb-20 max-w-lg mx-auto min-h-screen">
         <Outlet />
       </div>
