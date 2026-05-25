39336e7d28e1398a4979797b605c454d2ddd54d4
Dominic Billiau <dominicbilliau@gmail.com>
2026-05-25 00:06:25 +0000

    File changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
+// Register service worker early so notifications work as soon as permission is granted
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(() => {});
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
